a41e5588fc97a4229df2398ec6bd9f9d8ca215e9
nikhiljain1203 <121147663+nikhiljain1203@users.noreply.github.com>
2023-06-28 22:10:13 +0530

    Update Philosopher.java

--- Philosopher.java
+++ Philosopher.java
@@ -2,9 +2,12 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
-In this code, each philosopher is represented by a separate thread. The Philosopher class defines the behavior of each philosopher. They go through a cycle of thinking, picking up forks, eating, and putting down forks.
+In this code, each philosopher is represented by a separate thread. The Philosopher class defines the behavior of each philosopher. 
+They go through a cycle of thinking, picking up forks, eating, and putting down forks.
 
-The main method creates an array of Philosopher objects and an array of Lock objects representing the forks. Each philosopher is associated with a left fork and a right fork. The Lock objects ensure that only one philosopher can pick up a fork at a time, preventing deadlock.
+The main method creates an array of Philosopher objects and an array of Lock objects representing the forks. 
+Each philosopher is associated with a left fork and a right fork. 
+The Lock objects ensure that only one philosopher can pick up a fork at a time, preventing deadlock.
 
 When you run the code, you will see the output showing the actions of each philosopher, such as thinking, picking up forks, eating, and putting down forks.
 **/
